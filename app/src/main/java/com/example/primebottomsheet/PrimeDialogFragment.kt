043b35ce5aea88643e75922763f959e87733662a
Rahul Chaudhary <bossrc64@outlook.com>
2024-04-22 13:04:48 +0530

    Changed Linear layout to Coodinator Layout

--- app/src/main/java/com/example/primebottomsheet/PrimeDialogFragment.kt
+++ app/src/main/java/com/example/primebottomsheet/PrimeDialogFragment.kt
@@ -1,96 +1,40 @@
 package com.example.primebottomsheet
 
+import android.app.Dialog
 import android.os.Bundle
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import com.example.primebottomsheet.databinding.FragmentItemListDialogListDialogItemBinding
-import com.example.primebottomsheet.databinding.FragmentItemListDialogListDialogBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
-// TODO: Customize parameter argument names
-const val ARG_ITEM_COUNT = "item_count"
-
-/**
- *
- * A fragment that shows a list of items as a modal bottom sheet.
- *
- * You can show this modal bottom sheet from your activity like this:
- * <pre>
- *    ItemListDialogFragment.newInstance(30).show(supportFragmentManager, "dialog")
- * </pre>
- */
 class PrimeDialogFragment : BottomSheetDialogFragment() {
 
-    private var _binding: FragmentItemListDialogListDialogBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View? = inflater.inflate(R.layout.bottomsheet_fragment_dialog, container, false)
 
-        _binding = FragmentItemListDialogListDialogBinding.inflate(inflater, container, false)
-        return binding.root
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+//        val dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+//        dialog.setOnShowListener {
+//            val bottomSheetDialog = it as BottomSheetDialog
+//            val bottomSheet = bottomSheetDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
+//            bottomSheet?.let { sheet ->
+//                val behavior = BottomSheetBehavior.from(sheet)
+//                behavior.state = BottomSheetBehavior.STATE_EXPANDED
+//            }
+//        }
+//        dialog.apply {
+//            setStyle(STYLE_NORMAL , R.style.BottomSheetStyle)
+//        }
+        return BottomSheetDialog(requireContext(),R.style.BottomSheetStyle)
 
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        activity?.findViewById<RecyclerView>(R.id.list)?.layoutManager =
-//            LinearLayoutManager(context)
-//        activity?.findViewById<RecyclerView>(R.id.list)?.adapter =
-//            arguments?.getInt(ARG_ITEM_COUNT)?.let { ItemAdapter(it) }
-    }
-
-    private inner class ViewHolder internal constructor(binding: FragmentItemListDialogListDialogItemBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-
-        internal val text: TextView = binding.text
-    }
-
-    private inner class ItemAdapter internal constructor(private val mItemCount: Int) :
-        RecyclerView.Adapter<ViewHolder>() {
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-
-            return ViewHolder(
-                FragmentItemListDialogListDialogItemBinding.inflate(
-                    LayoutInflater.from(
-                        parent.context
-                    ), parent, false
-                )
-            )
-
-        }
-
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            holder.text.text = position.toString()
-        }
-
-        override fun getItemCount(): Int {
-            return mItemCount
-        }
     }
 
     companion object {
-
-        // TODO: Customize parameters
-        fun newInstance(itemCount: Int): PrimeDialogFragment =
-            PrimeDialogFragment().apply {
-                arguments = Bundle().apply {
-                    putInt(ARG_ITEM_COUNT, itemCount)
-                }
-            }
-
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        const val TAG = "ModalBottomSheet"
     }
 }
