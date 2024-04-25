e79c017e189cb75a5ec3f0f7ed9784ba24c73a3a
Rahul Chaudhary <bossrc64@outlook.com>
2024-04-25 17:08:14 +0530

    Added binding to bottomsheet fragment and setOnClickListener to skipAndBook and bePrimeBtn

--- app/src/main/java/com/example/primebottomsheet/PrimeDialogFragment.kt
+++ app/src/main/java/com/example/primebottomsheet/PrimeDialogFragment.kt
@@ -2,33 +2,38 @@ package com.example.primebottomsheet
 
 import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.primebottomsheet.databinding.BottomsheetFragmentDialogBinding
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class PrimeDialogFragment : BottomSheetDialogFragment() {
-
+    private lateinit var binding: BottomsheetFragmentDialogBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.bottomsheet_fragment_dialog, container, false)
+    ): View? {
+        binding = BottomsheetFragmentDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.skipAndBook.setOnClickListener {
+            Toast.makeText(requireContext(), "Skip and Book Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.bePrimeBtn.setOnClickListener {
+            Toast.makeText(requireContext(), "Be Prime Button Clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-//        val dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
-//        dialog.setOnShowListener {
-//            val bottomSheetDialog = it as BottomSheetDialog
-//            val bottomSheet = bottomSheetDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
-//            bottomSheet?.let { sheet ->
-//                val behavior = BottomSheetBehavior.from(sheet)
-//                behavior.state = BottomSheetBehavior.STATE_EXPANDED
-//            }
-//        }
-//        dialog.apply {
-//            setStyle(STYLE_NORMAL , R.style.BottomSheetStyle)
-//        }
         return BottomSheetDialog(requireContext(),R.style.BottomSheetStyle)
 
     }
@@ -37,3 +42,13 @@ class PrimeDialogFragment : BottomSheetDialogFragment() {
         const val TAG = "ModalBottomSheet"
     }
 }
+//
+//binding = BottomsheetFragmentDialogBinding.inflate(layoutInflater)
+//binding.skipAndBook.setOnClickListener {
+//    Toast.makeText(it.context, "Skip and Book Clicked", Toast.LENGTH_SHORT).show()
+//    Log.d("Prime Dialog Fragment", "Skip and Book Clicked")
+//}
+//binding.bePrimeBtn.setOnClickListener {
+//    Toast.makeText(it.context, "Be Prime Button Clicked", Toast.LENGTH_SHORT).show()
+//    Log.d("Prime Dialog Fragment", "Be Prime Clicked")
+//}
